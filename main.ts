e95b5c6c782db9953c43d50e9ea391af0e3ac4ec
timmegroenewegen <95966823+timmegroenewegen@users.noreply.github.com>
2023-10-24 07:28:53 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -14,15 +14,20 @@ basic.forever(function () {
     while (Obstakels.length > 0 && Obstakels[0].get(LedSpriteProperty.X) == 0) {
         Obstakels.removeAt(0).delete()
     }
-    for (let Obstakel of Obstakels) {
-        Obstakel.change(LedSpriteProperty.X, -1)
+    for (let Obstakel_2 of Obstakels) {
+        Obstakel_2.change(LedSpriteProperty.X, -1)
     }
     if (Ticks % 3 == 0) {
         Empty_obstakels = randint(0, 4)
-        for (let index = 0; index <= 4; index++) {
-            if (index != Empty_obstakels) {
-                Obstakels.push(game.createSprite(4, index))
+        for (let Index2 = 0; Index2 <= 4; Index2++) {
+            if (Index2 != Empty_obstakels) {
+                Obstakels.push(game.createSprite(4, Index2))
             }
+        }
+    }
+    for (let Obstakel_3 of Obstakels) {
+        if (Obstakel_3.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstakel_3.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
         }
     }
     Ticks += 1
